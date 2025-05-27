138202193993d55cf786107f260dfb69f70543a7
Basyrov Rustam <hrustbas@gmail.com>
2025-05-27 12:34:20 +0300

    Implement file list func

--- main.py
+++ main.py
@@ -2,7 +2,30 @@
 
 import os
 
-print("hello, world")
+def get_file_list(pwd, suffixes=None):
+    elements = os.listdir(pwd)
+    
+    # Ignore hidden dirs
+    elements = list(filter(lambda x: x[0] != '.', elements))
 
-curpath = os.getcwd()
+    # Add root prefix
+    elements = list(map(lambda x: os.path.join(pwd, x), elements))
 
+    files = list(filter(lambda x: os.path.isfile(x), elements))
+    dirs = list(filter(lambda x: os.path.isdir(x), elements))
+    
+    for directory in dirs:
+        files += get_file_list(directory)
+        pass
+
+    if not suffixes is None:
+        # TODO: Implement suffix filter
+        pass
+    
+    return sorted(files)
+
+
+# curpath = os.getcwd()
+
+files = get_file_list('.')
+print(files)
