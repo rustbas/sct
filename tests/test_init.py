fa017b4e02959eb1398cfbed26206fa6e649817e
Basyrov Rustam <hrustbas@gmail.com>
2025-06-01 16:36:43 +0300

    add one test

--- tests/test_init.py
+++ tests/test_init.py
@@ -7,5 +7,21 @@ import sct
 
 class TestInit:
     def test_init(self):
+        '''
+        Test empty class
+        '''
         test = sct.SCT('./tests')
         assert test.todos == []
+
+    def test_string(self):
+        '''
+        Test to_string function
+        '''
+        test = sct.Todo(
+                        status='status',
+                        file='file',
+                        row_number='row_number',
+                        task='task',
+                        priority='priority',
+                )
+        assert test.as_string == "file:row_number:task"
